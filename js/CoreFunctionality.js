aae20fdd5ff7a96ba516a51d4195b3934d71c473
Jan Andersson <jan.andersson.ie@gmail.com>
2021-08-24 18:03:22 +0100

    Esthetic

--- js/CoreFunctionality.js
+++ js/CoreFunctionality.js
@@ -1,6 +1,6 @@
 //Function for Generating dice number
 function NumGen() {
-  document.getElementById("GenNum").innerHTML = Math.floor(Math.random() * DiceNumber) + 1;
+  document.getElementById("DisplayDumber").innerHTML = Math.floor(Math.random() * DiceNumber) + 1;
    }
 
 /*
